80be5b06fa0d73507ab513ceb013a4a84367d160
Praveen Padashetty <praveen_padashetty@epam.com>
2024-01-24 22:02:55 +0530

    multi module maven project

---
+++ module1/src/test/java/com/module1/SampleTest.java
@@ -0,0 +1,12 @@
+package com.module1;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void setUp() {
+		
+	}
+
+}
---
+++ module2/src/test/java/com/module2/SampleTest.java
@@ -0,0 +1,12 @@
+package com.module2;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void setUp() {
+		
+	}
+
+}
